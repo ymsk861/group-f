d1b4002b4eb104df0a37efbc6de709ae01fcdf72
ymsk <yamasaki-shun219@g.ecc.u-tokyo.ac.jp>
2020-09-13 16:01:18 +0900

    heroku

--- app.py
+++ app.py
@@ -1,72 +1,118 @@
+import cv2
+import numpy as np
+import boto3
+import time
 from flask import Flask, render_template, request
 from wtforms import Form, FloatField, SubmitField, validators, ValidationError
-import numpy as np
+import matplotlib.pyplot as plt
 import joblib
+ 
 
-# 学習モデルを読み込み予測する
-def predict(parameters):
-    # モデル読み込み
-    model = joblib.load('./nn.pkl')
-    params = parameters.reshape(1,-1)
-    pred = model.predict(params)
-    return pred
+# setup 
+scale_factor = .15
+green = (0,255,0)
+red = (0,0,255)
+frame_thickness = 2
+cap = cv2.VideoCapture(0)
+rekognition = boto3.client('rekognition', region_name='us-west-2')
+fontscale = 1.0
+color = (0, 120, 238)
+fontface = cv2.FONT_HERSHEY_DUPLEX
 
-# ラベルからIrisの名前を取得
-def getName(label):
-    print(label)
-    if label == 0:
-        return "Iris Setosa"
-    elif label == 1: 
-        return "Iris Versicolor"
-    elif label == 2: 
-        return "Iris Virginica"
-    else: 
-        return "Error"
 
+# flask
 app = Flask(__name__)
 
-# Flaskとwtformsを使い、index.html側で表示させるフォームを構築する
-class IrisForm(Form):
-    SepalLength = FloatField("Sepal Length(cm)（蕚の長さ）",
-                     [validators.InputRequired("この項目は入力必須です"),
-                     validators.NumberRange(min=0, max=10)])
+dom_em_type = 'ワハハハ'
 
-    SepalWidth  = FloatField("Sepal Width(cm)（蕚の幅）",
-                     [validators.InputRequired("この項目は入力必須です"),
-                     validators.NumberRange(min=0, max=10)])
 
-    PetalLength = FloatField("Petal length(cm)（花弁の長さ）",
-                     [validators.InputRequired("この項目は入力必須です"),
-                     validators.NumberRange(min=0, max=10)])
+"""
+@app.route('/camera')
+    def bbb():
+        return render_template('')
+"""
+# @app.route('/',  methods = ['GET', 'POST'])
+def detect_face():
+    # while(True): # 下でreturnしているので表情を読み取るのは1回です
 
-    PetalWidth  = FloatField("petal Width(cm)（花弁の幅）",
-                     [validators.InputRequired("この項目は入力必須です"),
-                     validators.NumberRange(min=0, max=10)])
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    height, width, channels = frame.shape
 
-    # html側で表示するsubmitボタンの表示
-    submit = SubmitField("判定")
+    # Convert frame to jpg
+    small = cv2.resize(frame, (int(width * scale_factor), int(height * scale_factor)))
+    ret, buf = cv2.imencode('.jpg', small)
 
+    # Detect faces in jpg
+    faces = rekognition.detect_faces(Image={'Bytes':buf.tobytes()}, Attributes=['ALL'])
+
+    
+
+    # Draw rectangle around faces
+    for face in faces['FaceDetails']:
+        smile = face['Smile']['Value']
+        cv2.rectangle(frame,
+                    (int(face['BoundingBox']['Left']*width),
+                    int(face['BoundingBox']['Top']*height)),
+                    (int((face['BoundingBox']['Left']+face['BoundingBox']['Width'])*width),
+                    int((face['BoundingBox']['Top']+face['BoundingBox']['Height'])*height)),
+                    green if smile else red, frame_thickness)
+        emothions = face['Emotions']
+        i = 0
+        time.sleep(2)
+        for emothion in emothions:
+            cv2.putText(frame,
+                        str(emothion['Type']) + ": " + str(emothion['Confidence']),
+                        (25, 40 + (i * 25)),
+                        fontface,
+                        fontscale,
+                        color)
+            i += 1
+        dom_em_conf=emothions[0]['Confidence']
+        dom_em_type=emothions[0]['Type']
+        sec_em_conf=emothions[1]['Confidence']
+        sec_em_type=emothions[1]['Type']
+        
+
+        if dom_em_conf-sec_em_conf>30:
+            print(dom_em_type,dom_em_conf,'\n')
+        else:
+            print(dom_em_type,dom_em_conf)
+            print(sec_em_type,sec_em_conf,'\n')
+    return [dom_em_type, dom_em_conf]
+    #joblib.dump(dom_em_type, "dom_em_type.pkl", compress=True)
+
+    # When everything done, release the capture
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+    # return 
 @app.route('/', methods = ['GET', 'POST'])
-def predicts():
-    form = IrisForm(request.form)
-    if request.method == 'POST':
-        if form.validate() == False:
-            return render_template('index.html', form=form)
-        else:            
-            SepalLength = float(request.form["SepalLength"])            
-            SepalWidth  = float(request.form["SepalWidth"])            
-            PetalLength = float(request.form["PetalLength"])            
-            PetalWidth  = float(request.form["PetalWidth"])
+def aaa():
+    print(request.method)
+    if request.method == 'GET':
+        return render_template('index.html')
+    
+    # firstのスタートボタンを押すと実行
+    elif request.method == 'POST':
+        
+        # カメラ起動 + 認識
+        # emothions = detect_face()
+        # print(emothions)
+        dom_em_type = detect_face()
 
-            x = np.array([SepalLength, SepalWidth, PetalLength, PetalWidth])
-            pred = predict(x)
-            irisName = getName(pred)
-
-            return render_template('result.html', irisName=irisName)
-    elif request.method == 'GET':
-
-        return render_template('index.html', form=form)
+        # dom_em_type = joblib.load('./dom_em_type.pkl')
+        # 顔データ送信 emotions : [{'Type': 'CALM', 'Confidence': 92.8204574584961}, {'Type': 'SURPRISED', 'Confidence': 3.136558771133423}, ...]
+        return render_template('result.html', dom_em_type=dom_em_type)
 
 if __name__ == "__main__":
-    app.run()
+    app.run()     
 
+# カメラ画像を画面に表示したい場合は96行目に以下のコードを書く
+"""
+# Display the resulting frame
+cv2.imshow('frame', frame)
+if cv2.waitKey(1) & 0xFF == ord('q'):
+    break
+"""
